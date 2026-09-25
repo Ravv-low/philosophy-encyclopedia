28569fd2bb497202de59fd3179de2c15b68fb045
Rauf Ilham Andika <ilhamandikarauf@gmail.com>
2026-09-25 22:15:05 +0700

    Add files via upload

---
+++ js/data/index.js
@@ -0,0 +1,9 @@
+﻿import alFarabi from "./alfarabi.js";
+import aristotle from "./aristotle.js";
+import kant from "./kant.js";
+import foucault from "./foucault.js";
+import expandedProfiles from "./expanded.js";
+import expandedProfilesTwo from "./expanded2.js";
+
+export const philosophersData = [alFarabi, aristotle, kant, foucault, ...expandedProfiles, ...expandedProfilesTwo];
+export { alFarabi, aristotle, kant, foucault };
